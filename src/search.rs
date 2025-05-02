bafcbf07d1fdf802e50676a37fa4fbe4a7b3f623
adii <77727680+AdityaMotale@users.noreply.github.com>
2025-05-02 21:11:31 +0530

    creating hyperplanes

--- src/search.rs
+++ src/search.rs
@@ -1,3 +1,5 @@
+use rand::seq::IndexedRandom;
+
 #[derive(PartialEq, Eq, Hash)]
 pub struct HashKey<const N: usize>([u32; N]);
 
@@ -39,3 +41,63 @@ impl<const N: usize> Vector<N> {
     }
 }
 
+struct Hyperplane<const N: usize> {
+    coefficient: Vector<N>,
+    constant: f32,
+}
+
+impl<const N: usize> Hyperplane<N> {
+    fn point_is_above(&self, point: &Vector<N>) -> bool {
+        self.coefficient.dot_product(point) + self.constant >= 0.0
+    }
+}
+
+enum Node<const N: usize> {
+    Inner(Box<InnerNode<N>>),
+    Leaf(Box<LeafNode<N>>),
+}
+
+struct InnerNode<const N: usize> {
+    hyperplane: Hyperplane<N>,
+    left_node: Node<N>,
+    right_node: Node<N>,
+}
+
+struct LeafNode<const N: usize>(Vec<usize>);
+
+pub struct ANNIndex<const N: usize> {
+    ids: Vec<i32>,
+    trees: Vec<Node<N>>,
+    values: Vec<Vector<N>>,
+}
+
+impl<const N: usize> ANNIndex<N> {
+    fn build_hyperplane(
+        indexes: &Vec<usize>,
+        all_vecs: &Vec<Vector<N>>,
+    ) -> (Hyperplane<N>, Vec<usize>, Vec<usize>) {
+        let sample: Vec<_> = indexes.choose_multiple(&mut rand::rng(), 2).collect();
+
+        let (a, b) = (*sample[0], *sample[1]);
+        let coefficient = all_vecs[a].subtract_from(&all_vecs[b]);
+        let midpoint = all_vecs[a].avg(&all_vecs[b]);
+        let constant = -coefficient.dot_product(&midpoint);
+
+        let hyperplane = Hyperplane {
+            coefficient,
+            constant,
+        };
+
+        let (mut below, mut above) = (vec![], vec![]);
+
+        for &id in indexes.iter() {
+            if hyperplane.point_is_above(&all_vecs[id]) {
+                above.push(id);
+            } else {
+                below.push(id);
+            }
+        }
+
+        (hyperplane, above, below)
+    }
+}
