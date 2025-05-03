f609ee10be9dd96ec817e5ba060dd52a8d7ddad0
adii <77727680+AdityaMotale@users.noreply.github.com>
2025-05-03 13:52:06 +0530

    building an index

--- src/search.rs
+++ src/search.rs
@@ -1,4 +1,7 @@
+use std::collections::HashSet;
+
 use rand::seq::IndexedRandom;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 #[derive(PartialEq, Eq, Hash)]
 pub struct HashKey<const N: usize>([u32; N]);
@@ -99,5 +102,63 @@ impl<const N: usize> ANNIndex<N> {
         }
 
         (hyperplane, above, below)
+    }
+
+    fn build_tree(max_size: usize, indexes: &Vec<usize>, all_vecs: &Vec<Vector<N>>) -> Node<N> {
+        if indexes.len() <= max_size {
+            return Node::Leaf(Box::new(LeafNode::<N>(indexes.clone())));
+        }
+
+        let (hyperplane, above, below) = Self::build_hyperplane(indexes, all_vecs);
+
+        let left_node = Self::build_tree(max_size, &above, all_vecs);
+        let right_node = Self::build_tree(max_size, &below, all_vecs);
+
+        Node::Inner(Box::new(InnerNode::<N> {
+            hyperplane,
+            left_node,
+            right_node,
+        }))
+    }
+
+    fn deduplicate(
+        vecs: &Vec<Vector<N>>,
+        ids: &Vec<i32>,
+        dedup_vecs: &mut Vec<Vector<N>>,
+        dedup_ids: &mut Vec<i32>,
+    ) {
+        let mut seen_ids: HashSet<HashKey<N>> = HashSet::new();
+
+        for i in 1..vecs.len() {
+            let hash_key = vecs[i].to_hashkey();
+
+            if !seen_ids.contains(&hash_key) {
+                seen_ids.insert(hash_key);
+                dedup_vecs.push(vecs[i]);
+                dedup_ids.push(ids[i]);
+            }
+        }
+    }
+
+    pub fn build_index(
+        num_trees: usize,
+        max_size: usize,
+        vecs: &Vec<Vector<N>>,
+        ids: &Vec<i32>,
+    ) -> ANNIndex<N> {
+        let (mut unique_vecs, mut unique_ids) = (vec![], vec![]);
+        Self::deduplicate(vecs, ids, &mut unique_vecs, &mut unique_ids);
+
+        let all_indexes: Vec<usize> = (0..unique_vecs.len()).collect();
+        let trees: Vec<_> = (0..num_trees)
+            .into_par_iter()
+            .map(|_| Self::build_tree(max_size, &all_indexes, &unique_vecs))
+            .collect();
+
+        ANNIndex::<N> {
+            values: unique_vecs,
+            trees,
+            ids: unique_ids,
+        }
     }
 }
